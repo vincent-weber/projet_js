796f899f18fd7982cf7a9ba5d3b2f10efe53f4a4
Vincent <vincent.weber@etu.univ-amu.fr>
2018-03-02 14:41:35 +0100

    Ajout/modif de css et gestion des déplacements

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ A FAIRE > Créer un compte et faire en sorte qu'on puisse se connecter seulement
         > Puis essayer de faire un algo qui génère un labyrinthe automatiquement (avec bordures aléatoires QUI SONT DES CASES ET NON PAS DES BORDER !)
         > Pouvoir se déplacer dans le labyrinthe (clavier ou souris ?)
         > On démarre au début (en haut à gauche et on gagne quand on arrive à la fin (en bas à droite)
+        X event.which pour bouger le perso ?
 
 
  */
@@ -14,26 +15,23 @@ A FAIRE > Créer un compte et faire en sorte qu'on puisse se connecter seulement
 (function() {
     "use strict";
 
+    let css_perso = {
+        'background-color' : 'blue',
+        'border' : 'solid 2px black',
+    }
+
     let css_case = {
         'background-color' : 'white',
-        'text-align' : 'center',
-        'font-size' : 'large',
-        'border' : 'solid 2px red',
-        'color' : 'black'
+        'border' : 'solid 2px black'
     };
 
-    let css_rouge = {
+    let css_mur = {
         'background-color' : 'red',
-        'color' : 'white',
-        'font-size' : 'bolder',
-        'font-style' : 'oblique',
-        'text-align' : 'center'
+        'border' : 'solid 2px orange'
     };
 
     let css_table = {
         'width' : '750px',
-        //'background-color' : 'green',
-        'padding' : '0px 0%'
     }
 
     let css_center = {
@@ -43,23 +41,56 @@ A FAIRE > Créer un compte et faire en sorte qu'on puisse se connecter seulement
         'margin-bottom' : '85px'
     }
 
+
+
     $(document).ready(function () {
 
         let erreurCritique = function () {
             $('<body/>').html('Une erreur s\'est produite. Veuillez réessayer.')
         };
 
-        let habillerDamier = function (longueur, largeur) {
+        let habillerLaby = function (longueur, largeur) {
 
             $('#labyrinthe').css(css_table).css('height', 750*longueur/largeur);
             $('#labyrinthe.center').css(css_center);
-            //$('.test').css('width', 750/longueur + 'px').css('height', 750/largeur + 'px');
-            $(".case-laby").css(css_case).hover(function() {
-                $(this).css(css_rouge)
-            }, function() {
-                $(this).css(css_case);
-            });
+            $(".case-laby").css(css_case);
+            $(".case-perso").css(css_perso);
+            $(".case-mur").css(css_mur);
         };
+
+        let actualiserPositionPerso = function () {
+            $(".case-laby").css(css_case);
+            $(".case-perso").css(css_perso);
+        }
+
+
+        let initKeyboardEvents = function (laby_actuel) {
+            $('body').on('keydown', function (event) {
+
+                //Faire une fonction qui vérifie si il est possible de se déplacer dans la direction voulue (quand il y aura les murs)
+                var h_player = laby_actuel.player_hauteur;
+                var l_player = laby_actuel.player_largeur
+                if (event.keyCode === 83      && h_player !== laby_actuel.hauteur-1 && !laby_actuel.tab[h_player+1][l_player].td.hasClass('case-mur'))  // S
+                    laby_actuel.player_hauteur++;
+
+                else if (event.keyCode === 90 && h_player !== 0                     && !laby_actuel.tab[h_player-1][l_player].td.hasClass('case-mur'))  //Z
+                    laby_actuel.player_hauteur--;
+
+                else if (event.keyCode === 81 && l_player !== 0                     && !laby_actuel.tab[h_player][l_player-1].td.hasClass('case-mur'))  //Q
+                    laby_actuel.player_largeur--;
+
+                else if (event.keyCode === 68 && l_player !== laby_actuel.largeur-1 && !laby_actuel.tab[h_player][l_player+1].td.hasClass('case-mur'))  //D
+                    laby_actuel.player_largeur++;
+
+
+                laby_actuel.case_perso.td.removeClass('case-perso').addClass('case-laby');
+                laby_actuel.case_perso = laby_actuel.tab[laby_actuel.player_hauteur][laby_actuel.player_largeur];
+                laby_actuel.case_perso.td.removeClass('case-laby').addClass('case-perso');
+                actualiserPositionPerso();
+
+            });
+        }
+
 
         $.ajax({
             'url':'/json/est_connecte.php'
@@ -72,8 +103,9 @@ A FAIRE > Créer un compte et faire en sorte qu'on puisse se connecter seulement
                     $('#settings').show();
                     $('#userdeco').show();
                     if (typeof(data.laby_cree) !== "undefined") {
-                        new Labyrinthe(data.hauteur, data.largeur, '#labyrinthe');
-                        habillerDamier(data.hauteur, data.largeur);
+                        let laby_actuel = new Labyrinthe(data.hauteur, data.largeur, '#labyrinthe');
+                        habillerLaby(data.hauteur, data.largeur);
+                        initKeyboardEvents(laby_actuel);
                         $('#labyrinthe').show();
                     }
                 }
